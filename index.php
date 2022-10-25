b2b5f5b287e538cc249f5f8286feee2b3efe33fa
Alfred Matrimonio <30339882+pinoynukerz@users.noreply.github.com>
2022-10-25 11:53:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,23 +13,87 @@
 </head>
 
 <body>
-   <?php
-//ini_set('display_errors', 1 );
-ini_set('Show errors here', 1);
-error_reporting(E_ALL);
-//Write your Email Test address here
-$from = 'jmattz23@hotmail.com';
-//Write the email address you are sending the email to
-$to = 'jmattz23@hotmail.com';
-$subject = 'PHP Mail Test';
-$message = 'This is a simple test to check if your PHP Mail works';
-//Remember the header parameter is optional
-$headers = 'From:' . $from;
-mail($to, $subject, $message, $headers);
-$sent_mail = mail ($to, $subject, $message, $headers, '-fwebmaster@example.com');
-$error_message = 'Mail not sending';
-if ($sent_mail) echo 'Mail sent successfully!';
-else echo $error_message;
+ 
+<?php
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ *
+ *
+ *
+ */
+
+//require 'vendor/autoload.php';
+
+use Aws\Ses\SesClient; 
+use Aws\Exception\AwsException;
+
+//Create a SESClient 
+$SesClient = new Aws\Ses\SesClient([
+    'profile' => 'default',
+    'version' => '2010-12-01',
+    'region' => 'us-east-2'
+]);
+
+$html_body = '<h1>AWS Amazon Simple Email Service Test Email</h1>' .
+    '<p>This email was sent with <a href="https://aws.amazon.com/ses/">' .
+    'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">' .
+    'AWS SDK for PHP</a>.</p>';
+$subject = 'Amazon SES test (AWS SDK for PHP)';
+$plaintext_body = 'This email was send with Amazon SES using the AWS SDK for PHP.';
+$sender_email = 'jmattz23@hotmail.com';
+$recipient_emails = ['jmattz23@hotmail.com'];
+$char_set = 'UTF-8';
+$configuration_set = 'ConfigSet';
+
+try {
+    $result = $SesClient->sendEmail([
+        'Destination' => [
+            'ToAddresses' => $recipient_emails,
+        ],
+        'ReplyToAddresses' => [$sender_email],
+        'Source' => $sender_email,
+        'Message' => [
+
+            'Body' => [
+                'Html' => [
+                    'Charset' => $char_set,
+                    'Data' => $html_body,
+                ],
+                'Text' => [
+                    'Charset' => $char_set,
+                    'Data' => $plaintext_body,
+                ],
+            ],
+            'Subject' => [
+                'Charset' => $char_set,
+                'Data' => $subject,
+            ],
+        ],
+        // If you aren't using a configuration set, comment or delete the
+        // following line
+        'ConfigurationSetName' => $configuration_set,
+    ]);
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo "\n";
+}
+ 
+ 
+
+
 ?>
 
 </body>
